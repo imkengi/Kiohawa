a311233a1742a800c25d7967dac3160dd217bbf2
imkengi <48059971+imkengi@users.noreply.github.com>
2024-02-14 10:13:47 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "Babie của anh!",
+  text2: "Anh có điều này muốn hỏi Trang, Trang trả lời thật lòng nha ♥.",
+  text3: "Em có yêu anh honggg",
+  text4: "Nếu em không trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text5: "KHÔNG",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do em thích anh đi ",
+  text8: "Gửi cho anh <3",
+  text9: "Vì em yêu anh",
+  text10: "Anh yêu em Lê Vũ Diễm Trang ♥",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Anh hứa sẽ luôn yêu em, làm bạn gái anh nha bé yêu của anhhhhhhh",
   text12: "Okii lunn <3",
 };
 
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/trang.levudiem";
           },
         });
       }
